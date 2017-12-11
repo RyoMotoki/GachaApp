ee4af18ee24e38ab56a156e64fdc592630083321
Ryo Motoki <lalalararirureryo@gmail.com>
2017-12-11 20:31:36 +0900

    initial commit

--- GachaApp/ViewController.swift
+++ GachaApp/ViewController.swift
@@ -1,25 +1,43 @@
-//
-//  ViewController.swift
-//  GachaApp
-//
-//  Created by Ryo Motoki on 2017/10/18.
-//  Copyright © 2017年 RyoMotoki. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //変数の宣言(書いてもらうとこ)
+    var number = 0
+    @IBOutlet weak var otyaImageView: UIImageView!
+    @IBOutlet weak var gachaButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //モンスターの初期設定
+        otyaImageView.image = nil
+        //ボタンの初期設定
+        gachaButton.layer.cornerRadius = gachaButton.bounds.height / 2
+        gachaButton.layer.masksToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func tapGachaButton() {
+        //乱数の表示
+        number = Int(arc4random_uniform(100))
+        //ガチャのアルゴリズム(書いてもらうとこ)
+        if number < 60 {
+            otyaImageView.image = UIImage(named: "")
+        } else if number >= 60 && number < 80 {
+            otyaImageView.image = UIImage(named: "")
+        } else {
+            otyaImageView.image = UIImage(named: "")
+        }
+        //アニメーション
+        otyaImageView.alpha = 0.0
+        UIImageView.animate(withDuration: 1.0) {
+            self.otyaImageView.alpha = 1.0
+        }
+    }
+    
 }
-
